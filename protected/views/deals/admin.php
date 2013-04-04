008efbbf60a2b2bd7837d1bcedab9f9eb66ec433
root <root@yy-System-Product-Name>
2013-04-04 19:02:06 +0530

    added sms gateway and mail gateway controller actions

--- protected/views/deals/admin.php
+++ protected/views/deals/admin.php
@@ -1,3 +1,15 @@
+<style>
+#deals-grid .mail a {
+    background: none repeat scroll 0 0 #EC008C;
+    border-radius: 5px 5px 5px 5px;
+    color: #FFFFFF;
+    padding: 5px;
+}
+#deals-grid  .mail a:hover{
+      background: none repeat scroll 0 0 #38708A;
+}
+</style>
+
 <?php
 /* @var $this DealsController */
 /* @var $model Deals */
@@ -35,6 +47,7 @@ $('.search-form form').submit(function(){
     </style>
 <?php //echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
 <div class="createnew"><?php echo CHtml::link('Create New',array('create')); ?></div>
+<div id="mail"></div>
 <div class="search-form" style="display:none">
 <?php //$this->renderPartial('_search',array(
 	//'model'=>$model,
@@ -60,9 +73,15 @@ $active = array(1=>'Active',0=>'Inactive');
 //                'price',
 //		'offer_price',
 		//'valid',
-            
+                
 		array('header'=>'Valid Till','name'=>'valid','value'=>$data->valid),
-		array('name' => 'status','value'=>array($this,'getStatus'),'filter' => $active,'sortable'=>TRUE),
+		array('name' => 'status',
+                    'value'=>array($this,'getStatus'),
+                    'filter' => $active,'sortable'=>TRUE,
+//                    'cssClassExpression'=>'$data->id',
+
+                    'htmlOptions'=>array('class'=>'status'),
+                    ),
 		array
 			    (   
                 'name'=>'image',
@@ -75,6 +94,7 @@ $active = array(1=>'Active',0=>'Inactive');
                                 'htmlOptions'=>array('class'=>'thumb','rel'=>'gallery'),
 				
 			    ),
+           
 		array(
 			'class'=>'CButtonColumn',
 			'buttons'=>array
@@ -92,8 +112,58 @@ $active = array(1=>'Active',0=>'Inactive');
 				'imageUrl'=>Yii::app()->request->baseUrl.'/images/delete.png',
 				
 			    ),
+                            'mail' => array
+                                (   
+                                    
+                                    'label'=>'mail',
+                                    'imageUrl'=>Yii::app()->request->baseUrl.'/images/mail.png',
+                                    'url'=>'Yii::app()->createUrl("deals/sendmail", array("id"=>$data->id))',
+                                    'options' => array( 'ajax' => array(
+                                                                    'url'=>'js:$(this).attr("href")',
+                                                                    //'data'=> "js:$(this).serialize()",
+//                                                                    'success' => 'js:function(data) { $.fn.yiiGridView.update("deals-grid")}',
+                                                                    'beforeSend'=>'function(){
+                                                                       $("#mail").replaceWith("<div id=\"mail\"><img src=\"/images/loading.gif\"></div>");
+                                                                       
+
+
+                                                                        /* var obj= $(".mail");
+                                                                     obj.parent().parent("tr").find(".status").replaceWith("<td class=\"status\"><img src=\"/images/loading.gif\"></td>");*/
+                                                                     }',
+                                                                    'success'=>'function(data){
+                                                                        $("#mail").replaceWith("<div id=\"mail\">Mail Sent Successfully.</p>")
+                                                                        
+
+                                                                        /*$("#update_info").replaceWith("<p id=\"update_info\">Mail Sent Successfully.</p>")*/
+                                                                        }'),
+                                                         'class'=>'mail',
+                                        )),                                 
+                            'sms' => array
+                                (
+                                    'label'=>'sms',
+                                    'imageUrl'=>Yii::app()->request->baseUrl.'/images/sms.png',
+                                    'url'=>'Yii::app()->createUrl("deals/sendsms", array("id"=>$data->id))',
+                                    'options' => array( 'ajax' => array(
+                                                                    'url'=>'js:$(this).attr("href")',
+                                                                    //'data'=> "js:$(this).serialize()",
+//                                                                    'success' => 'js:function(data) { $.fn.yiiGridView.update("deals-grid")}',
+                                                                    'beforeSend'=>'function(){
+                                                                       $("#mail").replaceWith("<div id=\"mail\"><img src=\"/images/loading.gif\"></div>");
+                                                                        
+
+                                                                        /* var obj= $(".mail");
+                                                                     obj.parent().parent("tr").find(".status").replaceWith("<td class=\"status\"><img src=\"/images/loading.gif\"></td>");*/
+                                                                     }',
+                                                                    'success'=>'function(data){
+                                                                        $("#mail").replaceWith("<div id=\"mail\">SMS Sent Successfully.</p>")
+                                                                        
+
+                                                                        /*$("#update_info").replaceWith("<p id=\"update_info\">SMS Sent Successfully.</p>")*/
+                                                                        }'),
+                                                         'class'=>'mail',
+                                        )),
 			),
-			 'template'=>'{update}{delete}',
+			 'template'=>'{update}{delete}{mail}{sms}',
 		),
 	),
 )); ?>
