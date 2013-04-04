4480c940ca39d1e89c27310e6f9ae436bdd28e5f
root <root@yy-System-Product-Name>
2013-04-04 18:57:23 +0530

    added sms gateway

--- protected/models/Deals.php
+++ protected/models/Deals.php
@@ -43,7 +43,7 @@ class Deals extends CActiveRecord
 		return array(
 			array('merchant_id, title', 'required'),
 			array('merchant_id,offer_price, price, status', 'numerical', 'integerOnly'=>true),
-			array('title, description,terms', 'length', 'max'=>255),
+			array('title, description,terms', 'length', 'max'=>160),
 			array('valid', 'safe'),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
@@ -152,4 +152,56 @@ class Deals extends CActiveRecord
 
         return parent::beforeValidate ();
     }
+    public function sendSms($mobileno,$message){
+		
+		$request =""; //initialise the request variable
+		
+		$param[method]= "sendMessage";
+		
+		$param[send_to] = "91".$mobileno;
+		
+		$param[msg] = $message;
+		
+		$param[userid] = "2000033245";
+		
+		$param[password] = "shyam012";
+		
+		$param[v] = "1.1";
+		
+		$param[msg_type] = "TEXT"; //Can be "FLASH?/"UNICODE_TEXT"/?BINARY?
+		
+		$param[auth_scheme] = "PLAIN";
+		
+		//Have to URL encode the values
+		
+		foreach($param as $key=>$val) {
+		
+			$request.= $key."=".urlencode($val);
+			
+			//we have to urlencode the values
+			
+			$request.= "&";
+			
+			//append the ampersand (&) sign after each parameter/value pair
+		
+		}
+		
+		$request = substr($request, 0, strlen($request)-1);
+		
+		//remove final (&) sign from the request
+		
+		$url =
+		
+		"http://enterprise.smsgupshup.com/GatewayAPI/rest?".$request;
+		
+		$ch = curl_init($url);
+		
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		
+		$curl_scraped_page = curl_exec($ch);
+		
+		curl_close($ch);
+		
+		//echo $curl_scraped_page;
+	}
 }
