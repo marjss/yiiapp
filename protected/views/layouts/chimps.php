e1887df85d26ed4c17f5e2c6fbae34d9cd181c91
marjss <>
2013-05-16 11:50:48 +0530

    IE inclusion

--- protected/views/layouts/chimps.php
+++ protected/views/layouts/chimps.php
@@ -1,12 +1,13 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="en" xmlns:fb="http://www.facebook.com/2008/fbml">
     <head>
         <title><?php echo CHtml::encode($this->pageTitle); ?></title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>salonier</title>
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/landing.css" media="screen, projection" />
-        
+        <link rel="stylesheet" id="basename_css-css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/master.css" type="text/css" media="all">
         <?php Yii::app()->clientScript->registerCoreScript('jquery'); ?>
+        <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" type="text/css" media="screen">
         <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/plugins.js"></script>
         <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/scripts.js"></script>
 
@@ -14,9 +15,11 @@
                 <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/html5.js"></script>
                 <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" type="text/css" media="screen">
         <![endif]-->
+         <?php // $this->widget('Header'); ?>
              <?php $this->widget('Reviews');?>
 	  <?php $this->widget('RVMap');?>
 	 <?php $this->widget('Shopcontact');?>
+       
     </head>
     <body>
 
