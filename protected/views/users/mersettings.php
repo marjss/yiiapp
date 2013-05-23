66f6b9bab0727fd60aaff01eefc611a574edbe6f
marjss <>
2013-05-23 10:07:26 +0530

    review are added in settings page.

--- protected/views/users/mersettings.php
+++ protected/views/users/mersettings.php
@@ -5,35 +5,35 @@ $this->breadcrumbs=array(
 $this->widget('zii.widgets.CBreadcrumbs', array(
     'links'=>$this->breadcrumbs,
 ));
+$this->pageTitle = 'Salon Chimp - Settings';
 ?>
 <div class="merchantpanel">
     <h1>Settings</h1>
   <div class="mersettingup">
-    <?php echo CHtml::link('<div class="merchantholday"><h2>Holidays</h2></div>',Yii::app()->request->baseUrl.'/merholidays/admin');?>
-   <?php echo CHtml::link('<div class="merchantseats"><h2>Stylists</h2></div>',Yii::app()->request->baseUrl.'/merseats/admin');?>
-    
-    
-</div>
+   <?php echo CHtml::link('<div class="merchantholday"><h2>Holidays</h2></div>',Yii::app()->request->baseUrl.'/merholidays/admin');?>
+   <?php echo CHtml::link('<div class="merchantholday"><h2>Stylists</h2></div>',Yii::app()->request->baseUrl.'/merseats/admin');?>
+   <?php echo CHtml::link('<div class="merchantholday"><h2>Services/Products</h2></div>',Yii::app()->request->baseUrl.'/merservices/admin');?>
+  </div>
   <div class="clear"></div>
   <div class="mersettingup">
-     <?php echo CHtml::link('<div class="merchantholday"><h2>Services</h2></div>',Yii::app()->request->baseUrl.'/merservices/admin');?>
-    
-    <?php echo CHtml::link('<div class="merchantseats"><h2>Timings</h2></div>',Yii::app()->request->baseUrl.'/mertimings/admin');?>
-</div>
-    <div class="clear"></div>
-     <div class="mersettingup">
+    <?php echo CHtml::link('<div class="merchantholday"><h2>Timings</h2></div>',Yii::app()->request->baseUrl.'/mertimings/admin');?>
     <?php echo CHtml::link('<div class="merchantholday"><h2>Change Password</h2></div>',Yii::app()->request->baseUrl.'/users/changepassword');?>
-   <?php echo CHtml::link('<div class="merchantseats"><h2>Offers</h2></div>',Yii::app()->request->baseUrl.'/deals/admin');?>
+    <?php echo CHtml::link('<div class="merchantholday"><h2>Offers</h2></div>',Yii::app()->request->baseUrl.'/deals/admin');?>
 </div>
      <div class="clear"></div>
     <div class="mersettingup">
-    <?php echo CHtml::link('<div class="merchantholday"><h2>Description</h2></div>',Yii::app()->request->baseUrl.'/users/description');?>
-    <?php echo CHtml::link('<div class="merchantseats"><h2>Account</h2></div>',Yii::app()->request->baseUrl.'/users/account');?>    
-      <div class="clear"></div>  
-</div>
-      <div class="mersettingup">
+    <?php echo CHtml::link('<div class="merchantholday"><h2>About us</h2></div>',Yii::app()->request->baseUrl.'/users/description');?>
+    <?php echo CHtml::link('<div class="merchantholday"><h2>Profile</h2></div>',Yii::app()->request->baseUrl.'/users/account');?>    
     <?php echo CHtml::link('<div class="merchantholday"><h2>Gallery</h2></div>',Yii::app()->request->baseUrl.'/gallery/admin');?>
+    <div class="clear"></div>  
+</div>
+     <div class="mersettingup">
+    <?php echo CHtml::link('<div class="merchantholday"><h2>Reviews</h2></div>',Yii::app()->request->baseUrl.'/review/reviews');?>
+    <?php// echo CHtml::link('<div class="merchantholday"><h2>Profile</h2></div>',Yii::app()->request->baseUrl.'/users/account');?>    
+    <?php// echo CHtml::link('<div class="merchantholday"><h2>Gallery</h2></div>',Yii::app()->request->baseUrl.'/gallery/admin');?>
+    <div class="clear"></div>  
+</div>
+     
     
-      <div class="clear"></div>  
 </div>  
 </div>
